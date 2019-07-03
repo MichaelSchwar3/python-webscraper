004fcac0e75a8e60efabf0a81edf4559b520990c
Michael Schwartz <MichaelSchwar3@gmail.com>
2019-07-02 22:31:27 -0400

    Initial script run

---
+++ scrape.py
@@ -0,0 +1,35 @@
+import bs4
+import ssl
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+context = ssl._create_unverified_context()
+my_url = "http://www.mtggoldfish.com/metagame/standard#paper"
+
+#open the connection, grabbing the page
+uClient = uReq(my_url, context=context)
+
+#offloads content into variable
+page_html = uClient.read()
+
+#closes connection
+uClient.close()
+
+#html parsing
+page_soup = soup(page_html, "html.parser")
+#can do things like page_soup.h2 or page_soup.p to see relevant html tags
+
+containers = page_soup.findAll("div", {"class": "archetype-tile"})
+percentage = page_soup.findAll("td", {"class": "percentage"})
+container = containers[0]
+container.span
+
+for container in container:
+    deck_name = container.span.text.strip()
+    decks = container.table.td.text.strip()
+    percentage = container.findAll("td", {"class": "percentage"})[0].text.strip()
+    price = container.table.findAll(
+        "span", {"class": "deck-price-paper"})[0].text.strip().replace(u'\xa0', u' ')
+    print("Deck Name: " + deck_name)
+    print("Decks: " + decks)
+    print("Price: " + price)
+    print("Percentage played: " + price)
